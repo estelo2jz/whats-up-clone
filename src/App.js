429a021a48579e16f8bc993e08887959e1acba3d
esteloabell <manitobaaa@gmail.com>
2021-05-11 13:37:44 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Sidebar from './components/Sidebar';
+import Chat from './components/Chat';
+
+import './App.css';
+
+function App() {
+  return (
+    // BEM naming convention
+    <div className="app">
+      <div className="app__body">
+        <Sidebar />
+        <Chat />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/fisebase.js
@@ -0,0 +1,9 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyArhn8ux1whs00dqRJ3SNw9raAajn_uhDI",
+    authDomain: "whats-up-clone-a1a76.firebaseapp.com",
+    projectId: "whats-up-clone-a1a76",
+    storageBucket: "whats-up-clone-a1a76.appspot.com",
+    messagingSenderId: "538872149104",
+    appId: "1:538872149104:web:4eb8ea1317f1e18f7cb0b5",
+    measurementId: "G-XZVDRWKC2R"
+  };
